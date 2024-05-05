9412ebc09a36fc0fd65c88d8f9a5e5b00400f9af
chandan <chandanthakur7520@gmail.com>
2024-05-05 18:19:00 +0530

    while loop

---
+++ break.py
@@ -0,0 +1,6 @@
+#break statement in loop
+for i in range(10):
+    if (i==7):
+     break
+    print("5 X " , i , "=", 5 * (i))
+print("leave the loops")
--- whloop.py
+++ whloop.py
@@ -1,5 +1,4 @@
-i =int(input("enter the number: "))
-while (i<38):
-    i =int(input("enter the number: "))
-    print(i)
-print("done wth the loops")
+count = 2
+while (count<0):
+    print(count)
+    count = count + 1
