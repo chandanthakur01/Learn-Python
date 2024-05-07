839907b2b8956bb6996f7008d67ddd5789711fcd
chandan <chandanthakur7520@gmail.com>
2024-05-07 23:45:57 +0530

    function

---
+++ fn.py
@@ -0,0 +1,24 @@
+def calculateab(a, b):
+    cd=(a*b)/(a+b)
+    print(cd)
+
+def isgreater(a,b):
+    if (a>b):
+        print("first number is greater")
+    else:
+        print("second number is greater orequal")
+
+a = 8
+b = 6
+calculateab(a, b)
+isgreater(a,b)
+
+a = 7
+b = 7
+calculateab(a, b)
+isgreater(a,b)
+
+a = 8
+b = 7
+calculateab(a, b)
+isgreater(a, b)
