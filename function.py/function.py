ea487a20ef51150c25ed4302f20b4a9e21635df3
chandan <chandanthakur7520@gmail.com>
2024-05-08 20:27:33 +0530

    function

---
+++ function.py/function.py
@@ -0,0 +1,20 @@
+def calculateab(a, b):
+    ab=(a+b)/(a-b)
+    print(ab)
+
+    
+def value(a, b):
+    if (a>b):
+        print("first number is higher")
+    else:
+        print("second no is higher and equal")
+
+a = 7
+b = 3
+calculateab(a, b)
+value(a, b)
+
+
+a = 6
+b = 7
+value(a, b)
