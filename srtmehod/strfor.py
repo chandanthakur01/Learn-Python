e84ea1b137eb696ada5158ae7c653a4035288c84
chandan <chandanthakur7520@gmail.com>
2024-05-29 20:25:12 +0530

    str formating

---
+++ srtmehod/strfor.py
@@ -0,0 +1,11 @@
+name ="hii chandan how are {} and where are you {} "
+ab ="you"
+nm ="going"
+print(name.format(ab, nm))
+price = 17.27473
+txt=f"for only {price: .2f} dollars"
+print(txt)
+txt=f"for only {price: .3f} dollars"
+print(txt)
+txt = "for only {price:.2f} , {price: .3f}"
+print(txt.format(price =22.2232211))
